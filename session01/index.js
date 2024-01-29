6bd8cd50a56dbf59d4ae764c76bb65394f1e0c1c
Emilie2610 <156681783+Emilie2610@users.noreply.github.com>
2024-01-29 15:15:31 +0100

    first commit

---
+++ session01/index.js
@@ -0,0 +1,17 @@
+let nombreUN;
+let nombreDeux;
+let somme;
+
+nombreUN = 16;
+nombreDeux = 20;
+
+somme= nombreUN + nombreDeux;
+
+// functions
+
+function sum (x,y){
+
+    return x + y
+}
+
+console.log(sum(12, 30))
